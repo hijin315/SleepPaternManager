e5f8de98813e0680815c2f9a84a3b1ae0e7b2afb
heejin <h22_jin_030@naver.com>
2020-10-08 17:17:22 +0900

    mainfragment upgrade

--- app/src/main/java/com/example/sleeppaternmanager/MainFragment.java
+++ app/src/main/java/com/example/sleeppaternmanager/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.sleeppaternmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -22,7 +24,23 @@ public class MainFragment extends Fragment {
     }
 
     @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
+        AppCompatImageButton mainBtn1, mainBtn2;
+        mainBtn1 = (AppCompatImageButton)view.findViewById(R.id.mainBtn1);
+        mainBtn2 = (AppCompatImageButton)view.findViewById(R.id.mainBtn2);
+        mainBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Introduce.class);
+                startActivity(intent);
+            }
+        });
+        mainBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Notice.class);
+                startActivity(intent);
+            }
+        });
         int indicatorWidth = (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10,
                 getResources().getDisplayMetrics()) + 0.5f);
         int indicatorHeight = (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4,
---
+++ app/src/main/java/com/example/sleeppaternmanager/Notice.java
@@ -0,0 +1,15 @@
+package com.example.sleeppaternmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+
+public class Notice extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+    }
+}
